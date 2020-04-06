414938fc433df276ba0c30ad1c7145fcf97e8dd1
pawnlord-pjolumed <scruffduff321@gmail.com>
2020-04-06 14:51:30 -0400

    Can run commands correctly! Added SIGINT handler puts NULL at end of args for execution

--- shell.c
+++ shell.c
@@ -3,63 +3,93 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include  <signal.h>
+
+static volatile int running = 1;
+
+static volatile int in_command = 0;
 
 int run(char** args, int one_time) {
 	if(one_time) { /* this is one time, do not fork */
-		printf("ONE TIME!\n");
 		execvp(args[0], args);
-		return 0;
+		return -1;
 	}
+	int end_of_args;
+	for(end_of_args = 0; strcmp(args[end_of_args], ""); end_of_args++){} /* find end */
+	char* eoasave = args[end_of_args];
+	args[end_of_args] = NULL;
 	int process = fork();
 	if(process == 0) {	/* we are child process */
-		printf("CHILD EXECUTING!\n");
 		execvp(args[0], args);
+		exit(0);
 	} else if (process < 0) { /* we failed, exit */
-		perror("simpleshell: FORK ERROR! EXITING\n");
+		perror("simpleshell: run");
 		exit(EXIT_FAILURE);
 	} else { /* we are parent, wait until child exits */
-		printf("PARENT WAITING!\n");
+		in_command = 1;
 		wait(NULL);
+		in_command = 0;
 	}
+	printf("end: %d\n", args[1]);
+	args[end_of_args] = eoasave;
+	return process;
 }
 
 void init_args(char*** args, int s1, int s2){
-	char** array = (char**) malloc(sizeof(char*)*s1);
+	(*args) = malloc(sizeof(char*)*s1);
 	for(int i = 0; i < s1; i++) {
-		array[i] = (char*) malloc(sizeof(char)*s2);
+		(*args)[i] = malloc(sizeof(char)*s2);
 		for(int j = 0; j<s2; j++){
-			array[i][j]=0;
+			(*args)[i][j]=0;
 		}
 	}
-	*args = array;
+}
+void free_args(char*** args, int s1){
+	for(int i = 0; i < s1; i++) {
+		free(*args[i]);
+	}
+	free(*args);
 }
 void clear_args(char*** argsp){
-	char** args = *argsp;
-	for(int i = 0; strcmp(args[i], ""); i++){
-		for(int j; args[i][j] != 0; j++){
-			args[i][j] = 0;	
+	for(int i = 0; strcmp((*argsp)[i], ""); i++){
+		for(int j; (*argsp)[i][j] != 0; j++){
+			(*argsp)[i][j] = 0;	
 		}
 	}
-	*argsp = args;
+}
+
+void  INThandler(int sig) {
+	if(!in_command){
+		running = 0;
+	}
+}
+
+void start_handler(void (*handler)(int sig), int SIG) {
+    signal(SIG, handler);
 }
 
 
 int main(int argc, char** argv) {
 	char** args;
+	start_handler(INThandler, SIGINT);
+
 	if(argc > 1) {
 		args = argv+1;
 		run(args, 1);
 	}
+
+
 	init_args(&args, 10, 50);
-	while(1) {
-		init_args(&args, 10, 50);
+	while(running) {
+		clear_args(&args);
 		char* input = malloc(1000);
 		printf("simpleshell: ");
-		gets(input);
-		printf("Acquired input: %s\n", input);
+		fgets(input, 1000, stdin);
+		input[strcspn(input, "\n")] = '\0';
 		int current_arg = 0;
 		int args_char_counter = 0;
-		for(int i = 0; input[i] != 0; i++) {
+		int i;
+		for(i = 0; input[i] != 0; i++) {
 			if(input[i] == ' ') {
 				current_arg++;
 				args_char_counter = 0;
@@ -68,9 +98,11 @@ int main(int argc, char** argv) {
 				args_char_counter++;
 			}
 		}
-		args[++current_arg] = "";
-		if(strcmp(args[0], "exit") == 0)
+		strcpy(args[++current_arg], "");
+		if(strcmp(args[0], "exit") == 0){
 			break;
-		run(args, 0);
+		}
+		int d = run(args, 0);
+		free(input);
 	}
 }
